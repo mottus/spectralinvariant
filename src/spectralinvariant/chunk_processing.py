743077f7df92d999a3a92cff6c55d0237bc4551c
BijayKarki <50694588+BijayKarki@users.noreply.github.com>
2023-06-21 17:02:45 +0300

    wip: added pC function, seperated find_nearest

--- src/spectralinvariant/chunk_processing.py
+++ src/spectralinvariant/chunk_processing.py
@@ -13,28 +13,36 @@ from spectralinvariant.inversion import PROSPECT_D, pc_fast, minimize_cab, golde
 from spectralinvariant.spectralinvariants import p,  pC, referencealbedo_transformed, reference_wavelengths
 from spectralinvariant.hypdatatools_img import create_raster_like, get_wavelength
 
+def find_nearest(array, value):
+        """
+        Finds the index of array element closest to a given value
+        Used for finding the wavelengths 
+        """
+        idx = (np.abs(array - value)).argmin()
+        return idx
+
 
 def chunk_processing_p(hypfile_name, file_path, output_filename, chunk_size):
     """
-    This function computes spectral invariants using p() algorithm for a large (or any other) hypdata using the functions from hypdatatools_img.py module
+    Wraps p() function from hypdatatools_img.py module to process very large (or any other) file.
+    The input file is processed by splitting into chunks defined.
+    The input file is accessed using memmap. A raster of the same dimension is created and accessed likewise.
+    Based on the chunk size defined `p()` function is computed for the given data. 
     
-    The input file is accessed using memmap. A raster of the same dimension is created and accessed likewise. Based
-    on the user defined chunk size `p()` function is computed for the given data. 
-    
-    The results of the p() computation i.e. p, rho, DASF and R are stored in seperate column of the outputfile
-    for example, output_file_name[:,:, 0:4]
+    Args:
+        hypfile_name: ENVI header file,
+        file_path: file path
+        output_filename: file name for processed data
+        chunk_size: size of each chunk (int value)
+         
+    Result:
+        The results of the p() computation i.e. p, rho, DASF and R are stored in seperate column of the outputfile
+        for example, output_file_name[:,:, 0:4]
 
     """
     
     np.seterr(all="ignore")
-
-
-    def find_nearest(array, value):
-        """Finds the index of array element closest to a given value
-        """
-        idx = (np.abs(array - value)).argmin()
-        return idx
-    
+ 
     file_path = Path(file_path)
     if (file_path/hypfile_name).exists():
         print("Valid file name and file path.")
@@ -102,3 +110,91 @@ def chunk_processing_p(hypfile_name, file_path, output_filename, chunk_size):
     print()
     print(f"Process completed.\nComputation time = {round((time()-start_1)/60, 2)} mins.")
     
+
+def chunk_processing_pC(hypfile_name, file_path, output_filename, chunk_size):
+    """
+    Wraps pC() function from hypdatatools_img.py module to process very large (or any other) file.
+    The input file is processed by splitting into chunks defined.
+    The input file is accessed using memmap. A raster of the same dimension is created and accessed likewise.
+    Based on the chunk size defined `pC()` function is computed for the given data. 
+    
+    Args:
+        hypfile_name: ENVI header file,
+        file_path: file path
+        output_filename: file name for processed data
+        chunk_size: size of each chunk (int value)
+         
+    Result:
+    The results of the pC() computation i.e. p, rho, DASF,`$R^2$` and C are stored in seperate column of the outputfile
+    for example, output_file_name[:,:, 0:4]
+
+    """
+    np.seterr(all="ignore")
+       
+    file_path = Path(file_path)
+    if (file_path/hypfile_name).exists():
+        print("Valid file name and file path.")
+    else:
+        print("Please check file name and or file path!")
+        
+    # Reading the file as a numpy image
+    img = envi.open(file_path/hypfile_name)
+    
+    wavelength = get_wavelength(f'{str(file_path)}\{hypfile_name}')[0]
+        
+    input_image = img.open_memmap()
+    
+    # reading metadata
+    bands = bands = input_image.shape[2]
+    interleave_type = img.__dict__['metadata']['interleave']
+   
+    scale_factor = img.__dict__['scale_factor']
+    scale_factor = int(scale_factor)
+    
+    # creating a raster like file using create_raster_like function
+    outdata = create_raster_like(img, output_filename, Nlayers=bands, interleave=interleave_type, outtype=4, force=True)
+    outdata_map = outdata.open_memmap(writable=True)
+    
+    # Preparing inputs for pC function (670 >= lambda <= 790)
+
+    b1_pC = find_nearest(wavelength, 670)
+    b2 = find_nearest(wavelength, 790)
+       
+    ref_spectra_pC = np.interp(wavelength[b1_pC:b2+1], reference_wavelengths(), 0.5*referencealbedo_transformed())
+           
+    # Dimensions of the data
+    num_rows, num_cols, num_bands = input_image.shape
+
+    # Define the chunk size 
+    line = range(0, num_rows, chunk_size)
+    
+    print()
+    print(f"chunk size = {chunk_size}")
+    # Computing spectral invariants using pC() function
+    
+    start_1 = time()
+    start = start_1
+    for i in range(len(line)):
+        chunk = input_image[line[i]:line[i] + chunk_size, :, b1_pC:b2+1]
+        
+        if np.any(chunk!=0):            
+            float_chunk = chunk.astype('float')
+            nonzero_indices = float_chunk != 0
+            float_chunk[nonzero_indices] /= scale_factor
+        else:
+            float_chunk = chunk.astype('float')
+
+        processed_chunk = pC(float_chunk[:, :, :], ref_spectra_pC)
+
+        outdata_map[line[i]:line[i] + chunk_size, :, 0:5] = np.transpose(processed_chunk, (1,2,0))
+
+        if i > 0 and i % 10 == 0:
+            print(f'Line: {line[i]} / {num_cols}\nComputation time = {round((time()-start), 2)} secs.')
+            start = time()
+        else:
+            continue
+
+    outdata_map.flush() # writes and saves in the disk
+
+    print()
+    print(f"Process completed.\nComputation time = {round((time()-start_1)/60, 2)} mins.")
