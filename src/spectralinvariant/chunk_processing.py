a841b155e8b9d9dc38c27a30eb3cf44e6ec5ca03
BijayKarki <50694588+BijayKarki@users.noreply.github.com>
2023-07-14 11:37:34 +0300

    fix: compute_illumination_ function fixed and removed older version

--- src/spectralinvariant/chunk_processing.py
+++ src/spectralinvariant/chunk_processing.py
@@ -354,194 +354,11 @@ def create_chlorophyll_map(hypfile_name, hypfile_path, output_filename, inv_algo
     print(f'Cholorphyll map compuation completed!\nCompuation time = {(time() - start)/60:.2f} mins.')
     return 0
 
-def compute_inversion_and_illumination_correction(hypfile_name, hypfile_path, cmap_filename, cmap_file_path, output_filename_inv, output_filename_trueLR, chunk_size=None, wl_idx=None):
-
-    """
-    Wraps pc_fast function from inversion.py module on propspect_D model to compute spectral invariants from inverted chlorophyll map.
-    Similarly, illumination corrected leaf spectrum map is also computed.
-    Outputs two rasters of the same spatial dimension with the corresponding results.
-    
-    Args:
-        hypfile_name: ENVI header file (hyperspectral data),
-        hypfile_path: file path
-        cmap_filename: ENVI header file (chlorophyll map of the hypdata)
-        cmap_file_path: file path
-        
-        output_filename_inv: file name to store the results of pc_fast() computation i.e. p, rho and C.
-        output_filename_trueLR: file name for storing results of true leaf reflectance.
-                        
-        chunk_size: size of each chunk (int value). Default value = 3906250 or image resolution
-        wl_idx: index of wavelengths used in computations. Defaults to 670-720 nm
-         
-    Result:
-        returns 0 if the compuation is successfull, else -1.
-
-    """
-    
-    np.seterr(all="ignore")
-    functionname = "compute_inversion_and_illumination_correction()"
-    hyp_fullfilename = os.path.join(hypfile_path, hypfile_name)
-    cmap_fullfilename = os.path.join(cmap_file_path, cmap_filename)
-    
-    def check_file_exists(full_filename):
-        """
-        Prints error message and returns -1, if the file is not found
-        """
-        if not os.path.exists(full_filename):
-            print(functionname + " ERROR: file " + full_filename + "does not exist")
-            return -1
-
-    check_file_exists(hyp_fullfilename)
-    check_file_exists(cmap_fullfilename)
-    
-    #############################################
-    ###  Reading hypdata and chlorophyll map  ###
-    #############################################
-    
-    hyp_img = envi.open( hyp_fullfilename )
-    input_image = hyp_img.open_memmap()
-    
-    wavelength = get_wavelength(hyp_fullfilename )[0] # get_wavelength returns a tuple
-
-    if wl_idx is None:
-        wl_idx = [670, 720]
-
-    # sort the the list in ascending order just to be sure
-    wl_idx.sort()
-
-    # check the values of wl_idx are within the range of wavelength
-    is_wl_idx_valid =  wavelength[0] <= wl_idx[0] and wavelength[-1] >= wl_idx[-1]
-    
-    if not is_wl_idx_valid:
-        print(functionname + " ERROR: wavelength indices do not exist !")
-        return -1
-
-    b1_cab = find_nearest(wavelength, wl_idx[0]) 
-    b2_cab = find_nearest(wavelength, wl_idx[1])
-    wl_idx = np.arange( b1_cab, b2_cab+1 )
-
-    # Read metadata of hypdata
-    try:
-        scale_factor = hyp_img.__dict__['metadata']['reflectance scale factor'].astype(float)        
-    except:
-        scale_factor = 10000.0 # scale factor missing from metadata
-
-    # Dimensions of the hypdata
-    num_rows, num_cols, num_bands = input_image[:, :, :].shape
-    num_idx = num_rows * num_cols
- 
-    # Reading chlorophyll map
-    cab_img = envi.open(cmap_fullfilename )
-    cabs = cab_img.open_memmap()   
-
-    ##############################################################################################
-    ###  Creating  2 rasters for saving results from pc_fast() and corrected leaf reflectance  ###
-    ##############################################################################################        
-
-    # First raster for storing results from pc_fast() i.e, p, rho and C. 
-    output_band_names = {"band names": ("p", "rho", "C")}
-    num_output_layers = len(output_band_names['band names'])
-
-    description1 = "Inversion result computed using pc_fast() for " + hypfile_name + " "\
-        + str( wavelength[wl_idx[0]] ) + "-" + str( wavelength[wl_idx[-1]] ) + " nm."
-
-    outdata_1 = create_raster_like(hyp_img, output_filename_inv, description=description1,
-        Nlayers=num_output_layers, metadata_add=output_band_names, interleave='bip', outtype=4, force=True)
-
-    outdata_pc_fast = outdata_1.open_memmap(writable=True)
-    
-    # Second raster for storing results of true leaf reflectance.
-    description2 = "Corrected leaf reflectance for " + hypfile_name + " "\
-        + str( wavelength[0] ) + "-" + str( wavelength[-1] ) + " nm."
-
-    outdata_2 = create_raster_like(hyp_img, output_filename_trueLR, description=description2,
-        Nlayers=num_bands, metadata_keys_copy=['band names', 'wavelength' ], interleave='bip', outtype=4, force=True)
-
-    outdata_LR = outdata_2.open_memmap(writable=True)
-        
-  
-    ################################################
-    ###  Reshaping input and output files to 2D  ###
-    ################################################
-
-    input_image_linear = input_image[:, :, :].reshape(num_idx, num_bands)
-    input_image_subset_linear = input_image_linear[:, wl_idx[0]:wl_idx[-1]+1]
-    cabs_linear = cabs.reshape(num_idx)
-    
-    outdata_pc_fast = outdata_pc_fast.reshape(num_idx, num_output_layers)
-    outdata_LR = outdata_LR.reshape(num_idx, num_bands)
-    
-
-    # Creating an instance of the PROSPECT class with the input values specified by Ihalainen et al. (2023)
-    model = PROSPECT_D(N=1.5, Car=1.0, Cw=0.0, Cm=0.0)
-    model.subset(wavelength[wl_idx])
-    
-        
-    #################################
-    ###  Computing p, rho, and C  ###
-    #################################
-    
-    print()
-    print("Please wait! computing pc_fast() ....")
-
-    start = time()
-    start1 = start 
-
-    for i, pixel_cab in enumerate (input_image_subset_linear):
-        
-        pixel_cab = pixel_cab.astype('float')
-        pixel_cab /= scale_factor
-               
-        processed_pixel = pc_fast(pixel_cab, model.PROSPECT(Cab=cabs_linear[i]))
-        outdata_pc_fast[i, :] = processed_pixel
-        
-        if i > 0 and i % (num_idx // 10) == 0: # (added for testing)
-            print(f'Iteration: {i} / {len(input_image_linear)}\nComputation time = {(time()-start1)/60: .2f} mins.')
-            start1 = time()
-    
-
-    ################################
-    ###  Computing corrected LR  ###
-    ################################
-    
-    print()
-    print("Please wait! computing corrected leaf reflectance ....")
-    
-    if chunk_size == None:
-        chunk_size = np.round( 0.5e9 / 128).astype(int)
-        if chunk_size > num_idx:
-            chunk_size = num_idx
-    
-    chunk = np.arange(0, num_idx, chunk_size)
-    
-    for j in range (len(chunk)):
-        pixel = input_image_linear[chunk[j]:chunk[j]+chunk_size, :]
-        pixel = pixel.astype('float')
-        pixel /= scale_factor
-        
-        ps, rhos, cs =  outdata_pc_fast[chunk[j]:chunk[j]+chunk_size, 0],  outdata_pc_fast[chunk[j]:chunk[j]+chunk_size, 1],  outdata_pc_fast[chunk[j]:chunk[j]+chunk_size, 2]        
-        
-        corrected_LR = (pixel - cs[:, None]) / (rhos[:, None] + ps[:, None] * pixel)
-        outdata_LR[chunk[j]:chunk[j]+chunk_size, :] = corrected_LR
-    
-    
-    # converting output back to 3D shape        
-    outdata_pc_fast = outdata_pc_fast.reshape(num_rows, num_cols, num_output_layers)
-    outdata_pc_fast.flush() # writes and saves in the disk
-    
-    outdata_LR = outdata_LR.reshape(num_rows, num_cols, num_bands)
-    outdata_LR.flush() # writes and saves in the disk
-    
-    print()
-    print(f"{functionname}: process completed!\nProcess completion time = {(time() - start)/60:.2f} mins.")
-    
-    return 0
-
 def compute_inversion_invariants(hypfile_name, hypfile_path, cmap_filename, cmap_file_path, output_filename, wl_idx=None):
 
     """
     Wraps pc_fast function from inversion.py module on propspect_D model to compute spectral invariants from a chlorophyll map, produced using inversion algorithm.
-    The result is stored in a raster of the same spatial dimension with 3 layers.
+    The result is stored in a raster of the same spatial dimension with 3 layers in the current working directory.
        
     Args:
         hypfile_name: ENVI header file (hyperspectral data),
@@ -549,13 +366,13 @@ def compute_inversion_invariants(hypfile_name, hypfile_path, cmap_filename, cmap
         cmap_filename: ENVI header file (chlorophyll map of the hypdata created using inversion algorithm)
         cmap_file_path: file path
         
-        output_filename_inv: file name to store the results of pc_fast() computation i.e. p, rho and C        
+        output_filename_inv: file name to store the results of pc_fast() computation       
                         
         chunk_size: size of each chunk (int value). Default value = 3906250 pixel
         wl_idx: index of wavelengths used in computations. Defaults to 670-720 nm
          
     Result:
-        output file is stored in the current working directory 
+        output file with estimated values of rho, p and c  
         returns 0 if the compuation is successfull, else -1
 
     """
@@ -621,7 +438,7 @@ def compute_inversion_invariants(hypfile_name, hypfile_path, cmap_filename, cmap
     #############################################################        
 
     # First raster for storing results from pc_fast() i.e, p, rho and C. 
-    output_band_names = {"band names": ("p", "rho", "C")}
+    output_band_names = {"band names": ("rho", "p", "C")}
     num_output_layers = len(output_band_names['band names'])
 
     description1 = "Inversion result computed using pc_fast() for " + hypfile_name + " "\
@@ -650,7 +467,7 @@ def compute_inversion_invariants(hypfile_name, hypfile_path, cmap_filename, cmap
     
         
     #################################
-    ###  Computing p, rho, and C  ###
+    ###  Computing rho, p, and C  ###
     #################################
     
     print()
@@ -668,7 +485,7 @@ def compute_inversion_invariants(hypfile_name, hypfile_path, cmap_filename, cmap
         outdata_pc_fast[i, :] = processed_pixel
         
         if i > 0 and i % (num_idx // 10) == 0: # (added for testing)
-            print(f'Iteration: {i+1} / {len(input_image_linear)}\nComputation time = {(time()-start1)/60: .2f} mins.')
+            print(f'Iteration: {i+1} / {len(input_image_linear)}\nComputation time = {(time()-start1): .2f} seconds.')
             start1 = time()
         
     # converting output back to 3D shape        
@@ -683,12 +500,12 @@ def compute_inversion_invariants(hypfile_name, hypfile_path, cmap_filename, cmap
 def compute_illumination_corrected_leaf_spectra(hypfile_name, hypfile_path, inversion_filename, inversion_filepath, output_filename, chunk_size=None):
 
     """
-    Computes illumination corrected leaf spectrum using hypdata and spectral invariants (computed from inversion algorithm).
+    Computes illumination corrected leaf spectrum using hypdata and spectral invariants (computed from inversion algorithm i.e. `pc_fast()`).
         
     Args:
         hypfile_name: ENVI header file (hyperspectral data),
         hypfile_path: file path
-        inversion_filename: ENVI header file (result of inversion algorithm in the band/layer order of p, rho and c)
+        inversion_filename: ENVI header file (result of inversion algorithm in the band/layer order of rho, p and c)
         inversion_path: file path
         
         output_filename: file name for storing results of corrected leaf reflectance.
@@ -786,8 +603,8 @@ def compute_illumination_corrected_leaf_spectra(hypfile_name, hypfile_path, inve
         pixel = pixel.astype('float')
         pixel /= scale_factor
         
-        ps =  inversion_result_linear[ chunk[i]:chunk[i]+chunk_size, 0]
-        rhos = inversion_result_linear[ chunk[i]:chunk[i]+chunk_size, 1] 
+        rhos = inversion_result_linear[ chunk[i]:chunk[i]+chunk_size, 0] 
+        ps =  inversion_result_linear[ chunk[i]:chunk[i]+chunk_size, 1]        
         cs = inversion_result_linear[ chunk[i]:chunk[i]+chunk_size, 2]        
         
         corrected_LR = (pixel - cs[:, None]) / (rhos[:, None] + ps[:, None] * pixel)
@@ -835,7 +652,6 @@ def compute_correlation(array1, array2, method=None):
     # Method 1: Check for NaN values and remove it from both arrays
     if method == int(1):     
         mask = ~np.isnan(array1) & ~np.isnan(array2)
-        print("Dimension of the mask ", mask.shape)
         array1 = array1[mask]
         array2 = array2[mask]
 
